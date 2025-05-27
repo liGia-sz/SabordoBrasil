8f56daee57171eebe9956f3467891132c6d072ce
Ligia <s.morais.ligia@gmail.com>
2025-05-27 08:15:53 -0300

    redirecionamento para página userLogado

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ![Index](https://github.com/user-attachments/assets/03d88a36-48a5-4fe2-9435-168f80dc52ff)
 ![popup login](https://github.com/user-attachments/assets/5a762465-3f9f-47d0-b7c6-3302485c7779)
 ![Cadastro-pagina](https://github.com/user-attachments/assets/a991df6e-0643-4ed1-9e42-9a2256f83310)
+![alt text](user.logado.png)
--- obj/Debug/net8.0/SabordoBrasil.AssemblyInfo.cs
+++ obj/Debug/net8.0/SabordoBrasil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SabordoBrasil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5717b18fb6fe4f1889d103140a4d4b2ca5aa1440")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+316033eea72788f356b665d532810c76810d93f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SabordoBrasil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SabordoBrasil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
